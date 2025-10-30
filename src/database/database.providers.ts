f2a4b948727bba72466d1220b2bb111eeca85610
Vincent Lam <vincent@formfluid.com>
2025-10-30 12:29:36 +1030

    deploy: changed deployment settings

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,4 +1,4 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const databaseProviders = [
   {
@@ -13,13 +13,21 @@ export const databaseProviders = [
   },
 ];
 
-export const dataSource = new DataSource({
-  type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgress', 
-  database: process.env.DB_DATABASE || 'bookscroll',
-  entities: [process.env.NODE_ENV === 'development' ? 'src/**/*.entity{.ts,.js}' : 'dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
-});
+export const dataSource = new DataSource(
+  process.env.DATABASE_URL ? {
+    type: 'postgres',
+    entities: [process.env.NODE_ENV === 'development' ? 'src/**/*.entity{.ts,.js}' : 'dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+    url: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  } as DataSourceOptions : {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: 5432,
+    username: process.env.DB_USERNAME || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgress', 
+    database: process.env.DB_DATABASE || 'bookscroll',
+    entities: [process.env.NODE_ENV === 'development' ? 'src/**/*.entity{.ts,.js}' : 'dist/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  },
+);
